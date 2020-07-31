c20b7483e684880301d6f892692e0efa03979f8d
Ethan Widen <EDWiden96@gmail.com>
2020-07-31 09:47:10 -0500

    v1 Initial Upload (confirming)
    initial upload (confirming, but this should be v1)

---
+++ Plugins/com_dtschurch/Contactless-Checkin/QRExpressKioskAutoSelect.ascx
@@ -0,0 +1,19 @@
+﻿<%@ Control Language="C#" AutoEventWireup="true" CodeFile="QRExpressKioskAutoSelect.ascx.cs" Inherits="RockWeb.Plugins.com_DTS.ContactlessCheckin.QRExpressKioskAutoSelect" %>
+<asp:UpdatePanel ID="upnlContent" runat="server">
+    <ContentTemplate>
+
+        <div class="checkin-search-actions checkin-start">
+
+            <a class="btn btn-primary btn-checkin js-start-button" href="~/page/1529?KioskId=71&CheckinConfigId=14&GroupTypeIds=67%2C164%2C68%2C66%2C69%2C127%2C22&FamilyId={{PageParameter.FamilyId}}">
+                  <span>Check-in</span>
+               </a>
+            
+            </div>
+                <div class="checkin-search-actions checkin-start">
+            You should receive a text message with your Check-in QR code to the SMS enabled phone on your profile.  If you did not receive a text please come see the check-in counter at any kids check-in location.  <br /><br />
+            <small><em>**Pre Check-in does not save your spot in a room and is first come first serve.</em></small>
+
+        </div>
+
+    </ContentTemplate>
+</asp:UpdatePanel>
---
+++ Plugins/com_dtschurch/Contactless-Checkin/QRExpressKioskAutoSelect.ascx.cs
@@ -0,0 +1,139 @@
+﻿// <copyright>
+// Copyright by the Spark Development Network
+//
+// Licensed under the Rock Community License (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.rockrms.com/license
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+// </copyright>
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data.Entity;
+using System.Linq;
+using System.Text.RegularExpressions;
+using System.Web.UI;
+using System.Web.UI.HtmlControls;
+using System.Web.UI.WebControls;
+using com.DTSChurch.ContactlessCheckin;
+using Rock;
+using Rock.Attribute;
+using Rock.CheckIn;
+using Rock.Data;
+using Rock.Model;
+using Rock.Security;
+using Rock.Utility;
+using Rock.Web.Cache;
+using Rock.Web.UI;
+using Rock.Web.UI.Controls;
+
+namespace RockWeb.Plugins.com_DTS.ContactlessCheckin
+{
+    [DisplayName("QR Express Kiosk Auto Select")]
+    [Category("com_DTS > Check-in")]
+    [Description("Welcome screen with button that auto selects kiosk options")]
+
+    [MatrixField("bb85ba74-4ced-4939-afe7-3c4f7ae51de5", "Contactless Check-in Start Button Matrix", "This matrix defines the start buttons that will show on the welcome screen", true, "", 0, AttributeKey.ButtonSettingsMatrix)]
+    [LinkedPage("Welcome Page", "", true, "", "", 0, AttributeKey.WelcomePage)]
+
+    public partial class QRExpressKioskAutoSelect : RockBlock
+    {
+
+        #region Attribute Keys
+
+        private static class AttributeKey
+        {
+            public const string WelcomePage = "WelcomePage";
+            public const string ButtonSettingsMatrix = "ButtonSettingsMatrix";
+        }
+
+        #endregion Attribute Keys
+
+        #region PageParameterKeys
+
+        private static class PageParameterKey
+        {
+
+        }
+
+        #endregion PageParameterKeys
+
+        #region Fields
+
+        // used for private variables
+
+        #endregion
+
+        #region Properties
+
+        // used for public / protected properties
+
+        #endregion
+
+        #region Base Control Methods
+
+        //  overrides of the base RockBlock methods (i.e. OnInit, OnLoad)
+
+        /// <summary>
+        /// Raises the <see cref="E:System.Web.UI.Control.Init" /> event.
+        /// </summary>
+        /// <param name="e">An <see cref="T:System.EventArgs" /> object that contains the event data.</param>
+        protected override void OnInit(EventArgs e)
+        {
+            base.OnInit(e);
+
+            // this event gets fired after block settings are updated. it's nice to repaint the screen if these settings would alter it
+            this.BlockUpdated += Block_BlockUpdated;
+            this.AddConfigurationUpdateTrigger(upnlContent);
+        }
+
+        /// <summary>
+        /// Raises the <see cref="E:System.Web.UI.Control.Load" /> event.
+        /// </summary>
+        /// <param name="e">The <see cref="T:System.EventArgs" /> object that contains the event data.</param>
+        protected override void OnLoad(EventArgs e)
+        {
+            base.OnLoad(e);
+
+
+            if (!Page.IsPostBack)
+            {
+                // added for your convenience
+
+                // to show the created/modified by date time details in the PanelDrawer do something like this:
+                // pdAuditDetails.SetEntity( <YOUROBJECT>, ResolveRockUrl( "~" ) );
+            }
+        }
+
+        #endregion
+
+        #region Events
+
+        // handlers called by the controls on your block
+
+        /// <summary>
+        /// Handles the BlockUpdated event of the control.
+        /// </summary>
+        /// <param name="sender">The source of the event.</param>
+        /// <param name="e">The <see cref="EventArgs"/> instance containing the event data.</param>
+        protected void Block_BlockUpdated(object sender, EventArgs e)
+        {
+
+        }
+
+        #endregion
+
+        #region Methods
+
+        // helper functional methods (like BindGrid(), etc.)
+
+        #endregion
+    }
+}
